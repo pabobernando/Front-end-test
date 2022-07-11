88915e86bcb64aaa9d5404dc26ffb553eb1ce1e0
Pabo Bernando <pabobernando69@gmail.com>
2022-07-11 13:47:21 +0700

    ascending

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ const getApi = async () => {
   const poke = await fetch("https://pokeapi.co/api/v2/berry/")
   const value = await poke.json()
   // console.log(value)
-  const result = value.results.map(data => {
-    console.log(data)
+  const result = value.results
+  .sort((a, b) => a.name < b.name ? -1 : 1)
+  .map(data => {
     return {
       label: data.name,
       value: data.name
     }
   })
+
+  console.log(result)
   // console.log(result)
   setKumpulanData(result)
    
